0fc70deb32ebc8d56e21806800156d9ae477fde7
mac <mac.kittipat@gmail.com>
2016-10-16 10:14:18 +0700

    Change from some element to all element

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-$("html").css("filter", "none");
-$("body").css("filter", "none");
-$("img").css("filter", "none");
-$("div").css("filter", "none");
-$("span").css("filter", "none");
-$("p").css("filter", "none");
+$("*").css("filter", "none");
